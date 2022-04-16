ed45c66fe96699fa4fac00a83af431b9389d46b0
ci parkin <booshbrew@gmail.com>
2022-04-16 13:59:30 +0100

    commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
+var today = new Date;
+document.getElementById('time').innerHTML= today.toDateString();
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
@@ -12,10 +16,7 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-function displayDate() {
-    document.getElementById("showDate").innerHTML=Date();
 
-}
 
 var comment = document.getElementById("input");
 var saveButton = document.getElementById("save");
@@ -53,10 +54,31 @@ function init() {
 }
 init();
 
-var timeDisplayEl = $('#time-display');
 
+  function updateColour() {
+    // Sets interval in variable
+  
+  var currentHour = moment().hours();
+  
+  $('.time-colour').each(function () {
+      var hourColour = parseInt($(this).attr('id').split('-')[1]);
+  
+      // check if we've moved past this time
+      if (hourColour < currentHour) {
+          $(this).addClass("#past");
+        } else if (hourColour === currentHour) {
+          $(this).removeClass("#past");
+          $(this).addClass("#present");
+        } else {
+          $(this).removeClass("#past");
+          $(this).removeClass("#present");
+          $(this).addClass("#future");
+        }
+      });
+    }
+  
+  
+  updateColour()
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+    // set up interval to check if current time needs to be updated
+    var interval = setInterval(updateColour, 15000);
